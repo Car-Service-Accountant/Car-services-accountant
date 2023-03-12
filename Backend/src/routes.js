3848d757b194ae782c2fea46a9391ed53b8e14ba
GeorgiDishkov <georgidishkov2@gmail.com>
2023-03-12 15:27:58 +0200

    WIM

--- Backend/src/routes.js
+++ Backend/src/routes.js
@@ -1,8 +1,19 @@
 const router = require('express').Router();
+const authControler = require('./services/authServices.js');
 
 
+router.use("/auth" , async (req, res) => {
+    console.log(req.body);
+    const { username, email, password, rePassword } = req.body
+    try {
+        const token = await register(username, email, password, rePassword);
 
-
+        res.cookie('auth', token);
+        res.redirect('/')
+    } catch (error) {
+        return res.status(404).render('auth/register', { error: errorParser(error) });
+    }
+})
 router.all('*', (req, res) => {
     res.send("cusstom there")
 })
